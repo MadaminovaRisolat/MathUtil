e624b3e20099c118911d28590d1c135a4a4d4414
risolat0306 <madamrisolat@gmail.com>
2024-10-25 13:56:06 +0400

    MathUtil(updated)

--- src/MathUtil.java
+++ src/MathUtil.java
@@ -1,21 +1,102 @@
-
-
 public class MathUtil {
 
-    // 5 Non-static Methods
+    /**
+     * Checks whether a given number n is prime.
+     * @param n the number to check.
+     * @return true if the number is prime, otherwise false.
+     */
+    public static boolean isPrime(int n) {
 
-    public boolean isPerfectNumber(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) sum += i;
+        if (n <= 1) {
+            return false;
         }
-        return sum == n;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
+    /**
+     * Calculates the greatest common divisor (GCD) of two integers using the Euclidean algorithm.
+     * @param a first number
+     * @param b second number
+     * @return the GCD of a and b.
+     */
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    /**
+     * Computes the least common multiple (LCM) of two numbers.
+     * @param a first number
+     * @param b second number
+     * @return the LCM of a and b.
+     */
+    public static int lcm(int a, int b){
+        return a * b / gcd(a, b);
+    }
+
+
+    /**
+     * Returns the nth Fibonacci number.
+     * @param n index of the Fibonacci number.
+     * @return the nth Fibonacci number.
+     */
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+    }
+
+
+    /**
+     * Calculates the factorial of a number n.
+     * @param n the number to calculate factorial for.
+     * @return the factorial of n.
+     */
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return factorial(n - 1) + factorial(n - 2);
+    }
+
+    // non-static methods
+
+
+    /**
+     * Determines if the given number n is a perfect number.
+     * @param num the number to check.
+     * @return true if the number is perfect, otherwise false.
+     */
+    public boolean isPerfectNumber(int num) {
+        int sum = 1;
+        for(int i = 2 ; i <= num ; i++) {
+            if(num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
+
+    /**
+     * Computes the sum of the digits of the given number n.
+     * @param n the number to compute sum of digits for.
+     * @return the sum of the digits of n.
+     */
     public int sumOfDigits(int n) {
         int sum = 0;
-        while (n > 0) {
+        while(n != 0) {
             sum += n % 10;
             n /= 10;
         }
@@ -23,34 +104,44 @@ public class MathUtil {
     }
 
 
-    public int reverseNumber(int n) {
-        int reversed = 0;
-        while (n != 0) {
-            int digit = n % 10;
-            reversed = reversed * 10 + digit;
-            n /= 10;
+    /**
+     * Reverses the digits of a number n.
+     * @param num the number to reverse.
+     * @return the reversed number.
+     */
+    public int reverseNumber(int num) {
+        int reversed = 1;
+        while(num != 0) {
+            reversed *= 10;
+            num /= 10;
         }
         return reversed;
     }
 
 
-    public boolean isArmstrongNumber(int n) {
-        int original = n, sum = 0, digits = String.valueOf(n).length();
-        while (n > 0) {
-            int digit = n % 10;
-            sum += (int)Math.pow(digit, digits);
-            n /= 10;
+    /**
+     * Checks if a number is an Armstrong number.
+     * @param num the number to check.
+     * @return true if n is an Armstrong number, otherwise false.
+     */
+    public boolean isArmstrongNumber(int num) {
+        int sum = 0;
+        while(num != 0) {
+            sum += num % 10;
+            num /= 10;
         }
-        return sum == original;
+        return sum == num;
+    }
+
+    public int nextPrime(int num) {
+        int prime = num + 1;
+        while(isPrime(prime)) {
+            prime++;
+        }
+        return prime;
+
     }
 
 
-    public int nextPrime(int n) {
-        int candidate = n + 1;
-        while (!Main.isPrime(candidate)) {
-            candidate++;
-        }
-        return candidate;
-    }
 
 }
